c06244237c4872cc085d01b27e5dc68021b11534
telemanagerplus <jaiclaicliat@emeil.ir>
2016-08-22 14:17:47 +0430

    Update launch.sh

--- launch.sh
+++ launch.sh
@@ -7,10 +7,15 @@ if [ "$1" = "reattach" ]; then
 elif [ "$1" = "install" ]; then
   sudo apt-get install python-pip
   sudo pip install pyTelegramBotAPI
-  sudo pip install redis-server
+  sudo pip install redis
   sudo pip install telebot
   sudo pip install logging
   sudo pip install arrow
+  sudo pip install py==1.4.29
+  sudo pip install pytest==2.7.2
+  sudo pip install requests==2.7.0
+  sudo pip install six==1.9.0
+  sudo pip install wheel==0.24.0
   sudo apt-get install python2.7
   git clone https://github.com/telemanagerplus/python-api
   cd python-api
@@ -19,6 +24,7 @@ elif [ "$1" = "install" ]; then
 elif [ "$1" = "fix" ]; then
   git clone https://github.com/telemanagerplus/TeleBot.git
   sudo apt-get install urllib
+  sudo apt-get innstall redis-server
   sudo service start redis-server
 
 
